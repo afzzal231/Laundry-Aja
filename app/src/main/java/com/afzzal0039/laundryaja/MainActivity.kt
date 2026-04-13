a3cfecb29feab79398a8c39779f011a869182c42
afzzal231 <afzzalfaizalrabby@student.telkomuniversity.ac.id>
2026-04-13 11:02:01 +0700

    Implementasi navigasi pada MainActivity
    - Menghapus kode boilerplate Greeting dan konfigurasi Scaffold default.
    - Menambahkan `rememberNavController` untuk mengelola navigasi.
    - Mengintegrasikan `NavGraph` ke dalam `MainActivity` sebagai pengatur alur navigasi aplikasi.

--- app/src/main/java/com/afzzal0039/laundryaja/MainActivity.kt
+++ app/src/main/java/com/afzzal0039/laundryaja/MainActivity.kt
@@ -3,45 +3,19 @@ package com.afzzal0039.laundryaja
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.afzzal0039.laundryaja.Navigation.NavGraph
 import com.afzzal0039.laundryaja.ui.theme.LaundryAjaTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             LaundryAjaTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                val navController = rememberNavController()
+                NavGraph(navController = navController)
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LaundryAjaTheme {
-        Greeting("Android")
-    }
-}
